06b6ec0b3dc8ad42757471167b0785338cd437e1
mohssinekissane <kissanemohssine2001@gmail.com>
2025-10-27 13:20:39 +0100

    selection des produits

--- lib/produit_box.dart
+++ lib/produit_box.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ProduitBox extends StatelessWidget {
-  const ProduitBox({super.key, required this.nomProduit});
+  const ProduitBox({
+    super.key, 
+    required this.nomProduit,
+    required this.selProduit,
+    required this.onChanged
+  });
   
   final String nomProduit;
+  final bool selProduit;
+  final Function onChanged;
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 120,
-      child: Text(nomProduit)
+      child: Row(
+        children: [
+          Checkbox(
+            value: selProduit,
+            onChanged: (bool? value) {
+              onChanged(value);
+            },
+          ),
+          Text(nomProduit),
+        ],
+      ),
     );
   }
 }
--- lib/produits_list.dart
+++ lib/produits_list.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'produit_box.dart';
 
-class ProduitsList extends StatelessWidget {
+class ProduitsList extends StatefulWidget {
   const ProduitsList({super.key});
 
-  final List liste = const [
-    ["1 produit",false],
-    ["2 produit",false],
-    ["3 produit",false],
-    ["4 produit",false],
-    ["5 produit",false]
+  @override
+  State<ProduitsList> createState() => _ProduitsListState();
+}
+
+class _ProduitsListState extends State<ProduitsList> {
+  List liste = [
+    ["1 produit", false],
+    ["2 produit", false],
+    ["3 produit", false],
+    ["4 produit", false],
+    ["5 produit", false]
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +26,17 @@ class ProduitsList extends StatelessWidget {
       body: ListView.builder(
         itemCount: liste.length,
         itemBuilder: (context, index) {
-          return ProduitBox(nomProduit: liste[index][0]);
+          return ProduitBox(
+            nomProduit: liste[index][0],
+            selProduit: liste[index][1],
+            onChanged: (bool? value) {
+              setState(() {
+                liste[index][1] = value ?? false;
+              });
+            },
+          );
         },
       ),
     );
-   
   }
 }
